0b5d5d572328ee76664cfdbe9c7f7b01024fd474
Krasimir Kraev <kraevit@gmail.com>
2020-08-01 22:57:51 +0300

    Update backup-script-cron.sh

--- backup-script-cron.sh
+++ backup-script-cron.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-BACKUP_TIME=`date +%b-%d-%y` #get the current date
+# Get the Current Date
+BACKUP_TIME=`date +%b-%d-%y`
 
-SOURCE=/home/usr/path/data_folder #the folder that contains the files that we want to backup
+# The Folder that Contains the Files that We Want to Backup
+SOURCE="/home/usr/path/data_folder "
 
-DESTINATION=/home/usr/path/backup-$BACKUPTIME.tar.gz #create a backup file using the current date in it's name
+# Create a Backup File Using the Current Date in It's Name
+DESTINATION="/home/usr/path/backup-${BACKUP_TIME}.tar.gz"
 
-tar -cpzf $DESTINATION $SOURCEFOLDER #create the backup
+# Create the Backup
+tar -cpzf $DESTINATION $SOURCEFOLDER 
 
-### Run this commands in terminal to setup cronjob ###
-######################################################
-# Let’s say that we want to run the script everyday at 12:30 a.m. we would type
+######################################
+# How to Run the Script as a CRONJOB ?
+######################################
 
+# Let’s Say that We Want to Run the Script Everyday at 12:30A.M. We Would Type
+#
 # $ crontab -e
 # $ 29 0 * * * /bin/bash /path/backup_script.sh
 
-# 29 stands for the 30 minute
-# 0 stands for 12 a.m.
-# The first * stands for everyday, the second * stands for every month, the third * stands for every week day.
+# More Info
+#
+# 29 Stands for the 30 Minute
+# 0 Stands for 12A.M.
+# The First * Stands for Everyday
+# The Second * Stands for Every Month
+# The Third * Stands for Every Week Day
+#
+# END
